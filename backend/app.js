1c1bb8447ecd76164d25733232d1cd7d3f502651
jitain <jitain6d@gmail.com>
2025-11-20 19:52:59 +0530

    Lab Project Starts

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const pool = require('./config/db');
+
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
